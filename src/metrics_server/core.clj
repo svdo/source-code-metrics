604974d0ae4f83fa82c90b58a9adf893748773ab
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-06-03 10:35:44 +0200

    Makes it more functional by removing @config atom

--- src/metrics_server/core.clj
+++ src/metrics_server/core.clj
@@ -8,17 +8,18 @@
             [clojure.edn :as edn]))
 
 (def config (atom nil))
+(def auth-token (atom nil))
 
-(defn basic-auth [] {:basic-auth (str (:token @config) ":")})
+(defn basic-auth [] {:basic-auth (str @auth-token ":")})
 (defn url [endpoint] (str "<redacted>" endpoint))
 (def measures-component-tree "/api/measures/component_tree")
 
-(defn categorize [entry]
+(defn categorize-complexity [orange-threshold red-threshold entry]
   (let [value (:value entry)]
     (cond
-      (< value (:complexity-orange-threshold @config)) :green
-      (< value (:complexity-red-threshold @config))    :orange
-      :else                                            :red)))
+      (< value orange-threshold) :green
+      (< value red-threshold)    :orange
+      :else                      :red)))
 
 (defn raw-metric-page [project-id metric page page-size]
   (client/get (url measures-component-tree)
@@ -32,8 +33,8 @@
   (> (* pageIndex pageSize) total))
 
 (defn fetch-metric
-  ([project-id metric] (fetch-metric project-id metric 1 (:page-size @config)))
-  ([project-id metric page page-size]
+  ([project-id metric page-size] (fetch-metric project-id metric page-size 1))
+  ([project-id metric page-size page]
    (let [response (raw-metric-page project-id metric page page-size)
          measures (:body response)
          parsed (json/read-str measures :key-fn keyword)
@@ -42,11 +43,15 @@
      (if (is-last-page (:paging parsed))
        this-page
        (reduce conj
-               (fetch-metric project-id metric (inc page) page-size)
+               (fetch-metric project-id metric page-size (inc page))
                this-page)))))
 
-(defn categorize-complexity []
-  (let [components (fetch-metric (:project-id @config) "complexity")
+(defn fetch-categorized-complexity [config]
+  (let [project-id (:project-id config)
+        page-size (:page-size config)
+        orange (:complexity-orange-threshold config)
+        red (:complexity-red-threshold config)
+        components (fetch-metric project-id "complexity" page-size)
         interesting-part (map (fn [{:keys [key name]
                                     [{:keys [value]}] :measures}]
                                 {:key key
@@ -57,7 +62,7 @@
          (filter #(some? (:value %)))
          (filter #(not (str/includes? (:name %) "/")))
          (map #(update-in % [:value] read-string))
-         (map #(assoc % :category (categorize %)))
+         (map #(assoc % :category (categorize-complexity orange red %)))
          (sort-by :value)
          (reverse)
          (group-by :category))))
@@ -75,10 +80,11 @@
       (printf "Error parsing edn file '%s': %s\n" source (.getMessage e)))))
 
 (defn -main []
-  (reset! config (load-edn "config.edn"))
-  (let [complexity (categorize-complexity)]
-    ;; (pprint complexity)
-    (println "Complexity:")
-    (println "  green:" (count (:green complexity)))
-    (println "  orange:" (count (:orange complexity)))
-    (println "  red:" (count (:red complexity)))))
+  (let [config (load-edn "config.edn")]
+    (reset! auth-token (:token config))
+    (let [complexity (fetch-categorized-complexity config)]
+      ;; (pprint complexity)
+      (println "Complexity:")
+      (println "  green:" (count (:green complexity)))
+      (println "  orange:" (count (:orange complexity)))
+      (println "  red:" (count (:red complexity))))))
