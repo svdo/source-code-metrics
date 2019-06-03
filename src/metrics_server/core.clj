7365e7590733257be49ffb6eac2f008da5a9f790
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-06-03 15:36:37 +0200

    Attempts better readability by extracting functions and removes atom for auth-token
    (with apologies for a commit that is too big)

--- src/metrics_server/core.clj
+++ src/metrics_server/core.clj
@@ -1,27 +1,25 @@
 (ns metrics-server.core
   (:gen-class)
   (:require [clj-http.client :as client]
-            ; [clojure.pprint :refer [pprint]]
             [clojure.string :as str]
             [clojure.data.json :as json]
             [metrics-server.config :refer (load-config)]))
 
-(def auth-token (atom nil))
-
-(defn basic-auth [] {:basic-auth (str @auth-token ":")})
+(defn basic-auth [token] {:basic-auth (str token ":")})
 (defn url [endpoint] (str "<redacted>" endpoint))
 (def measures-component-tree "/api/measures/component_tree")
 
-(defn categorize-complexity [orange-threshold red-threshold entry]
+(defn categorize-complexity
+  [entry {:keys [complexity-orange-threshold complexity-red-threshold]}]
   (let [value (:value entry)]
     (cond
-      (< value orange-threshold) :green
-      (< value red-threshold)    :orange
-      :else                      :red)))
+      (< value complexity-orange-threshold) :green
+      (< value complexity-red-threshold)    :orange
+      :else                                 :red)))
 
-(defn raw-metric-page [project-id metric page page-size]
+(defn raw-metric-page [project-id metric page page-size token]
   (client/get (url measures-component-tree)
-              (merge (basic-auth)
+              (merge (basic-auth token)
                      {:query-params {:component project-id
                                      :metricKeys metric
                                      :p page
@@ -31,47 +29,53 @@
   (> (* pageIndex pageSize) total))
 
 (defn fetch-metric
-  ([project-id metric page-size] (fetch-metric project-id metric page-size 1))
-  ([project-id metric page-size page]
-   (let [response (raw-metric-page project-id metric page page-size)
+  ([metric config] (fetch-metric metric config 1))
+  ([metric config page]
+   (let [project-id (:project-id config)
+         page-size (:page-size config)
+         token (:token config)
+         response (raw-metric-page project-id metric page page-size token)
          measures (:body response)
          parsed (json/read-str measures :key-fn keyword)
          this-page (:components parsed)]
-     ;; (pprint (:paging parsed))
      (if (is-last-page (:paging parsed))
        this-page
        (reduce conj
-               (fetch-metric project-id metric page-size (inc page))
+               (fetch-metric metric config (inc page))
                this-page)))))
 
-(defn compute-categorized-complexity [config metrics-data]
-  (let [orange (:complexity-orange-threshold config)
-        red (:complexity-red-threshold config)
-        interesting-part (map (fn [{:keys [key name]
-                                    [{:keys [value]}] :measures}]
-                                {:key key
-                                 :name name
-                                 :value value})
-                              metrics-data)]
+(defn extract-relevant-fields [{:keys [key name]
+                                [{:keys [value]}] :measures}]
+  {:key key
+   :name name
+   :value value})
+
+(defn value-string-to-number [entry]
+  (update-in entry [:value] read-string))
+
+(defn has-a-value [entry]
+  (some? (:value entry)))
+
+(defn is-a-file [entry]
+  (not (str/includes? (:name entry) "/")))
+
+(defn compute-categorized-complexity [metrics-data config]
+  (let [interesting-part (map extract-relevant-fields metrics-data)]
     (->> interesting-part
-         (filter #(some? (:value %)))
-         (filter #(not (str/includes? (:name %) "/")))
-         (map #(update-in % [:value] read-string))
-         (map #(assoc % :category (categorize-complexity orange red %)))
+         (filter has-a-value)
+         (filter is-a-file)
+         (map value-string-to-number)
+         (map #(assoc % :category (categorize-complexity % config)))
          (sort-by :value)
          (reverse)
          (group-by :category))))
 
 (defn -main []
-  (let [config (load-config)]
-    (reset! auth-token (:token config))
-    (let [project-id (:project-id config)
-          page-size (:page-size config)
-          metrics-data (fetch-metric project-id "complexity" page-size)
-          complexity (compute-categorized-complexity config metrics-data)]
-      ;; (pprint complexity)
-      (println "Project key:" (:project-id config))
-      (println "Complexity:")
-      (println "  green:" (count (:green complexity)))
-      (println "  orange:" (count (:orange complexity)))
-      (println "  red:" (count (:red complexity))))))
+  (let [config (load-config)
+        metrics-data (fetch-metric "complexity" config)
+        complexity (compute-categorized-complexity metrics-data config)]
+    (println "Project key:" (:project-id config))
+    (println "Complexity:")
+    (println "  green:" (count (:green complexity)))
+    (println "  orange:" (count (:orange complexity)))
+    (println "  red:" (count (:red complexity)))))
