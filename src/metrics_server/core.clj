23bf7db78ffcc9bfb4a76f85943776b57580b684
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-06-04 16:41:52 +0200

    Completes first two sections of metrics Excel sheet

--- src/metrics_server/core.clj
+++ src/metrics_server/core.clj
@@ -7,15 +7,17 @@
 
 (defn -main []
   (let [config (load-config)
-        project-data (sonar/fetch-project-metrics ["coverage" "new_coverage"] config)
+        project-data (sonar/fetch-project-metrics ["files" "complexity" "coverage" "new_coverage"] config)
         project-metrics (project/metrics project-data)
         complexity-data (sonar/fetch-file-tree-metric "complexity" config)
         complexity (complexity/categorize complexity-data config)]
     (println "Project key:" (:project-id config))
-    (println "Coverage:")
-    (println "  overall:" (format "%.1f" (:coverage project-metrics)))
-    (println "  new code:" (format "%.1f" (:new-coverage project-metrics)))
     (println "Complexity:")
+    (println "  Number of files:" (:files project-metrics))
+    (println "  Sum of complexity:" (:complexity project-metrics))
     (println "  green:" (count (:green complexity)))
     (println "  orange:" (count (:orange complexity)))
-    (println "  red:" (count (:red complexity)))))
+    (println "  red:" (count (:red complexity)))
+    (println "Coverage:")
+    (println "  overall:" (format "%.1f" (:coverage project-metrics)))
+    (println "  new code:" (format "%.1f" (:new-coverage project-metrics)))))
