717c068c681a2e43882d9ec2dd279bee8e580716
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-06-07 18:16:12 +0200

    Moves `load-edn` utils to separate ns

--- src/metrics_server/config.clj
+++ src/metrics_server/config.clj
@@ -1,23 +1,5 @@
 (ns metrics-server.config
-  (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]))
-
-(defn load-edn
-  "Load edn from an io/reader source (filename or io/resource)."
-  [source]
-  (try
-    (with-open [r (io/reader source)]
-      (edn/read (java.io.PushbackReader. r)))
-
-    (catch java.io.IOException e
-      (printf "Couldn't open '%s': %s\n" source (.getMessage e)))
-    (catch RuntimeException e
-      (printf "Error parsing edn file '%s': %s\n" source (.getMessage e)))))
-
-(defn load-edn! [source]
-  (try
-    (load-edn source)
-    (catch Exception e {})))
+  (:require [metrics-server.load-edn :refer (load-edn load-edn!)]))
 
 (defn load-config []
   (let [config (load-edn "config.edn")
