94c667642e1f18541c24b223adb7ed07a38e015f
Stefan van den Oord <svdo@users.noreply.github.com>
2019-07-05 16:01:58 +0200

    Validates metrics data

--- src/metrics_server/project_metrics.clj
+++ src/metrics_server/project_metrics.clj
@@ -1,5 +1,7 @@
 (ns metrics-server.project-metrics
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.spec.alpha :as s]
+            [expound.alpha :as e]))
 
 (defn- value-string-to-number [dict]
   (into {} (for [[k v] dict] [(keyword (str/replace (name k) "_" "-"))
@@ -9,8 +11,32 @@
   {(keyword (:metric metric-dict))
    (or (:value metric-dict) (:value (first (:periods metric-dict))))})
 
+(s/def :metrics/value
+  (s/and string? #_(comp (or int? float?) read-string)
+         (s/or :int-string (comp int? read-string)
+               :float-string (comp float? read-string))))
+(s/def :metrics/period
+  (s/keys :opt-un [:metrics/value]))
+(s/def :metrics/periods (s/coll-of :metrics/period))
+(s/def :metrics/measure
+  (s/and
+   (s/keys :req-un [::metric]
+           :opt-un [:metrics/value
+                    :metrics/periods])
+   (s/or :measure-value #(:value %)
+         :first-period-value #(:value (first (:periods %))))))
+(s/def :metrics/measures (s/coll-of :metrics/measure))
+(s/def :metrics/data
+  (s/keys :req-un [:metrics/measures]))
+
+(defn valid-data [metrics-data]
+  (if (s/valid? :metrics/data metrics-data)
+    metrics-data
+    (throw (ex-info (e/expound-str :metrics/data metrics-data)
+                    (s/explain-data :metrics/data metrics-data)))))
+
 (defn metrics [metrics-data]
-  (->> (:measures metrics-data)
+  (->> (:measures (valid-data metrics-data))
        (map extract-relevant-fields)
        (map value-string-to-number)
        (reduce merge)))
