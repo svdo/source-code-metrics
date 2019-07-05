7386e52f1de849cdaf9d8ce3427f2e4e0a58daa5
Stefan van den Oord <svdo@users.noreply.github.com>
2019-07-05 14:21:45 +0200

    Adds validation of incoming data

--- src/metrics_server/churn.clj
+++ src/metrics_server/churn.clj
@@ -1,4 +1,6 @@
-(ns metrics-server.churn)
+(ns metrics-server.churn
+  (:require [clojure.spec.alpha :as s]
+            [expound.alpha :as e]))
 
 (defn- extract-relevant-fields [{{:keys [additions deletions]} :stats}]
   {:additions additions
@@ -7,7 +9,21 @@
 (defn- is-not-merge-commit? [commit-entry]
   (= 1 (count (:parent_ids commit-entry))))
 
+(s/def :commit/stats
+  (s/keys :req-un [::additions ::deletions]))
+(s/def :commit/entry
+  (s/keys :req-un [::parent_ids :commit/stats]))
+(s/def :commit/entries
+  (s/coll-of :commit/entry))
+
+(defn- check-valid [commit-data]
+  (if (s/valid? :commit/entries commit-data)
+    :ok
+    (throw (ex-info (e/expound-str :commit/entries commit-data)
+                    (s/explain-data :commit/entries commit-data)))))
+
 (defn summarize [commit-data]
+  (check-valid commit-data)
   (let [interesting-part (->> commit-data
                               (filter is-not-merge-commit?)
                               (map extract-relevant-fields))
