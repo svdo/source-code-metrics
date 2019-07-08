cd406c5dcb77eb37c9f5548338128953b130939a
Stefan van den Oord <svdo@users.noreply.github.com>
2019-07-08 13:44:58 +0200

    Uses property-based testing

--- test/metrics_server/complexity_test.clj
+++ test/metrics_server/complexity_test.clj
@@ -1,11 +1,21 @@
 (ns metrics-server.complexity-test
   (:require [clojure.test :refer (deftest testing is)]
             [clojure.spec.alpha :as s]
-            [clojure.spec.gen.alpha :as gen]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer (defspec)]
             [metrics-server.complexity :as complexity]))
 
 (def config {:complexity/orange-threshold 10
              :complexity/red-threshold    20})
+
+(defspec complexity-spec 100
+  (prop/for-all
+    [sample (s/gen :complexity/entries)]
+    (let [categorized (complexity/categorize sample config)]
+      (and (:green categorized)
+           (:orange categorized)
+           (:red categorized)))))
 
 (deftest complexity-test
   (testing "complexity boundaries"
@@ -13,20 +23,6 @@
       (is (= :red (categorize-complexity-number {:value 25} config)))
       (is (= :orange (categorize-complexity-number {:value 15} config)))
       (is (= :green (categorize-complexity-number {:value 5} config)))))
-
-  (testing "it contains green"
-    (let [sample (gen/generate (s/gen :complexity/entries))]
-      (is (:green (complexity/categorize sample config)))))
-
-  (testing "it contains orange"
-    (let [sample (gen/generate (s/gen :complexity/entries))
-          categorized (complexity/categorize sample config)]
-      (is (:orange categorized))))
-
-  (testing "it contains red"
-    (let [sample (gen/generate (s/gen :complexity/entries))
-          categorized (complexity/categorize sample config)]
-      (is (:red categorized))))
 
   (testing "it throws on invalid input"
     (is (thrown? RuntimeException (complexity/categorize [{}] config)))))
